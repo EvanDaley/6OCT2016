f7e347b785003c6004cd246dde1bbcee53b61472
Evan <daley.evan@gmail.com>
2016-10-06 20:56:39 -0700

    Implement Unity Ads

--- 6-OCT-2016/Assets/RollABall/Scripts/Ball.cs
+++ 6-OCT-2016/Assets/RollABall/Scripts/Ball.cs
@@ -7,18 +7,61 @@ public class Ball : MonoBehaviour {
 	private Rigidbody rbody;
 	public float moveForce;
 
+	private float stabilityModifierDuration = 4f;
+	private float stabilityModifierCooldown = 0;
+	private bool stabilityModifierActive = false;
+
+	private float startingDrag;
+	public float modifiedDrag = 3;
+	public float stabilityModifierSpeedIncrease = 2f;
+
 	void Start () 
 	{
 		startPos = transform.position;
 		rbody = GetComponent<Rigidbody> ();
+
+		startingDrag = rbody.drag;
 	}
-	
+
+	public bool StabilityModifierActive
+	{
+		get { return stabilityModifierActive; }
+		set 
+		{
+			stabilityModifierActive = value;
+
+			if (value == true)
+			{
+				rbody.drag = modifiedDrag;
+			} else
+			{
+				rbody.drag = startingDrag;
+			}
+		}
+	}
+
+	public float StabilityModifier
+	{
+		get { 
+			if (StabilityModifierActive)
+				return stabilityModifierSpeedIncrease;
+			else
+				return 0;
+		}
+	}
+
+	public void EngageStabilityModifier()
+	{
+		stabilityModifierActive = true;
+		stabilityModifierCooldown = Time.time + stabilityModifierDuration;
+	}
+		
 	void Update () 
 	{
 		float x = Input.acceleration.x;
 		float y = Input.acceleration.y;
 
-		rbody.AddForce (new Vector3 (x, 0, y) * moveForce);
+		rbody.AddForce (new Vector3 (x, 0, y) * moveForce * StabilityModifier);
 	}
 
 	void OnCollisionEnter(Collision collision)
