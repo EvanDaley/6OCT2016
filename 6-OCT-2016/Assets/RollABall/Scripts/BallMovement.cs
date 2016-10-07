94f7ea7a3cf0aa765aa396013757335c6f25d26f
Evan <daley.evan@gmail.com>
2016-10-06 21:04:34 -0700

    Simplify movement controls

---
+++ 6-OCT-2016/Assets/RollABall/Scripts/BallMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallMovement : MonoBehaviour {
+
+	private Vector3 startPosition;
+	private Rigidbody rbody;
+	public float moveForce;
+
+	void Start()
+	{
+		startPosition = transform.position;
+		rbody = GetComponent < Rigidbody>();
+	}
+
+	void Update () {
+		float x = -Input.acceleration.y;
+		float y = Input.acceleration.x;
+		rbody.AddForce(new Vector3 (x, 0, y) * moveForce);
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.layer == 9)
+		{
+			gameObject.transform.position = startPosition;
+		}
+	}
+}
