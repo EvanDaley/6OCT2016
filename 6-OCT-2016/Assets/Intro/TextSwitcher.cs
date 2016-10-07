f3a9672edc55f2e7549957b0b4b18419b6545ec2
Evan <daley.evan@gmail.com>
2016-10-06 17:48:32 -0700

    Improve intro and create title menu

---
+++ 6-OCT-2016/Assets/Intro/TextSwitcher.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextSwitcher : MonoBehaviour {
+	
+	public Canvas Canvas;
+	public GameObject TextPrefab;
+	public PlainText[] plainTextList;
+
+	void Update () 
+	{
+		// if time is passed the startTime of any objects and they haven't been created, create them and remove them from the list
+		for(int i = 0; i < plainTextList.Length; i++)
+		{
+			if (plainTextList [i] .show == true)
+			{
+				if (Time.time > plainTextList[i].startTime)
+				{
+					GameObject textInstance = GameObject.Instantiate (TextPrefab, Vector3.zero, transform.rotation) as GameObject;
+					textInstance.transform.SetParent (Canvas.transform, false);
+
+					DieAfterTime deathTimer = textInstance.GetComponent<DieAfterTime> ();
+					deathTimer.SetDeathTime (plainTextList[i].endTime);
+
+					Text UIText = textInstance.GetComponent<Text> ();
+					UIText.text = plainTextList[i].text;
+
+					plainTextList [i].show = false;
+				}
+			}
+		}
+	}
+}
+
+[System.Serializable]
+public class PlainText
+{
+	// position
+	// scroll effect
+
+	public bool show = true;
+
+	public string text = "";
+	public float startTime = 0f;
+	public float endTime = 0f;
+}
